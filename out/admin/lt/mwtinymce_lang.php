3cd1c431ca4e43b4a38d2ebb7a92bd1b206f3fb4
Edvinas Kručas <edv.krucas@gmail.com>
2013-07-17 14:03:53 +0300

    Added LT translations

---
+++ out/admin/lt/mwtinymce_lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * English language-file for OXID-Module "mwtinymce"
+ *
+ * @package oxid-modules\mwtinymce
+ */
+
+$sLangName  = "Lietuvių";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset' => 'UTF-8',
+
+  'SHOP_MODULE_GROUP_mwtinymce'     => 'Nustatymai',
+  'SHOP_MODULE_sMwTinyMceUploadDir' => 'Failų įkėlimo katalogas',
+  'SHOP_MODULE_aMwTinyMceEnabledClasses' => 'Klasės, kurioms turi būti taikomas TinyMCE redaktorius',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE" }]
+*/
