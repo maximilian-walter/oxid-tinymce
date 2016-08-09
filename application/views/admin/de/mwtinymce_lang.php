852002b0448d3a68ee9230c51311fae29cbf837a
Maximilian Walter <github@max-walter.net>
2016-08-09 18:26:17 +0200

    Moved translation files

---
+++ application/views/admin/de/mwtinymce_lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * German language-file for OXID-Module "mwtinymce"
+ *
+ * @package oxid-modules\mwvideo
+ */
+
+$sLangName  = "Deutsch";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+  'charset' => 'UTF-8',
+
+  'SHOP_MODULE_GROUP_mwtinymce'          => 'Allgemein',
+  'SHOP_MODULE_sMwTinyMceUploadDir'      => 'Upload-Verzeichnis',
+  'SHOP_MODULE_aMwTinyMceEnabledClasses' => 'Klassen in denen der TinyMCE Editor eingebunden wird',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE" }]
+*/
---
+++ application/views/admin/en/mwtinymce_lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * English language-file for OXID-Module "mwtinymce"
+ *
+ * @package oxid-modules\mwtinymce
+ */
+
+$sLangName  = "English";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+  'charset' => 'UTF-8',
+
+  'SHOP_MODULE_GROUP_mwtinymce'          => 'Main',
+  'SHOP_MODULE_sMwTinyMceUploadDir'      => 'Upload-Directory',
+  'SHOP_MODULE_aMwTinyMceEnabledClasses' => 'Classes to apply TinyMCE editor',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE" }]
+*/
